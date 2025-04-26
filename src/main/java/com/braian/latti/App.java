daf865bb805443c1436c9aca409d16ff543afbd5
BraianMelo <Braian.ptu@gmail.com>
2025-04-26 10:23:10 -0300

    Sistema de arquivos redefinido

--- src/main/java/com/braian/latti/App.java
+++ src/main/java/com/braian/latti/App.java
@@ -17,6 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+    	System.out.println(App.class.getResource("fxmls/primary.fxml").toString());
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
@@ -27,7 +28,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("fxmls/"+ fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
